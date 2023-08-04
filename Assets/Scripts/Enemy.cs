d95094cd80471b786298141f045521abfb8bc449
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-08-04 15:03:37 +0900

    add: instantiate in runtime

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    GameObject explosionVFX;
+
+    [SerializeField]
+    Transform parent;
+
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"I'm {this.name}. Hit by {other.gameObject.name}");
-
+        // Debug.Log($"I'm {this.name}. Hit by {other.gameObject.name}");
+        GameObject vfx = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
