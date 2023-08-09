2fb0a265d602521938a55b615e6bddbad7297e99
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-08-09 13:09:37 +0900

    update: score changing text

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        // scoreText.text = "Start";
+
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
